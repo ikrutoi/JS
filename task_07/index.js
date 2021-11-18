39f2a9dd2195f14eeb95803c185c860cb2209260
Igor <iharkrutoi@gmail.com>
2021-11-18 21:32:55 +0300

    Task 07

---
+++ task_07/index.js
@@ -0,0 +1,20 @@
+const speech = prompt('Введите пожалуйста строку');
+let sum = 0;
+
+function searchVowel(sp) {
+  for (let i = 0; sp[i] != undefined; i++) {
+    if (sp[i] == 'а' || sp[i] == 'А' ||
+      sp[i] == 'е' || sp[i] == 'Е' ||
+      sp[i] == 'ё' || sp[i] == 'Ё' ||
+      sp[i] == 'и' || sp[i] == 'И' ||
+      sp[i] == 'о' || sp[i] == 'О' ||
+      sp[i] == 'э' || sp[i] == 'Э' ||
+      sp[i] == 'ю' || sp[i] == 'Ю' ||
+      sp[i] == 'я' || sp[i] == 'Я')
+    sum++; 
+  }
+}
+
+searchVowel(speech);
+
+console.log(sum)
