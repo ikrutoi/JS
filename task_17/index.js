9a0120ed17872d335cf42e974f69972c49d425bf
Igor <iharkrutoi@gmail.com>
2021-12-15 15:58:05 +0300

    task 17 new

--- task_17/index.js
+++ task_17/index.js
@@ -11,42 +11,38 @@ newArray(speech);
 
 //  Использование метода массива forEach:
 
-// arr.forEach(it => {
-//   if (it == 'а' || it == 'А' ||
-//   it == 'е' || it == 'Е' ||
-//   it == 'ё' || it == 'Ё' ||
-//   it == 'и' || it == 'И' ||
-//   it == 'о' || it == 'О' ||
-//   it == 'э' || it == 'Э' ||
-//   it == 'ю' || it == 'Ю' ||
-//   it == 'я' || it == 'Я')
-//   sum++; 
-// });
+arr.forEach(it => {
+  if (it == 'а' || it == 'А' ||
+  it == 'е' || it == 'Е' ||
+  it == 'ё' || it == 'Ё' ||
+  it == 'и' || it == 'И' ||
+  it == 'о' || it == 'О' ||
+  it == 'э' || it == 'Э' ||
+  it == 'ю' || it == 'Ю' ||
+  it == 'я' || it == 'Я')
+  sum++; 
+});
 
-// console.log(sum);
+console.log(sum);
 
 // Использование метода массива filter:
 
-// function filterArray(it) {
-//   return it == 'а' || it == 'А' ||
-//     it == 'е' || it == 'Е' ||
-//     it == 'ё' || it == 'Ё' ||
-//     it == 'и' || it == 'И' ||
-//     it == 'о' || it == 'О' ||
-//     it == 'э' || it == 'Э' ||
-//     it == 'ю' || it == 'Ю' ||
-//     it == 'я' || it == 'Я';
-// };
+function filterArray(it) {
+  return it == 'а' || it == 'А' ||
+    it == 'е' || it == 'Е' ||
+    it == 'ё' || it == 'Ё' ||
+    it == 'и' || it == 'И' ||
+    it == 'о' || it == 'О' ||
+    it == 'э' || it == 'Э' ||
+    it == 'ю' || it == 'Ю' ||
+    it == 'я' || it == 'Я';
+};
 
-// sum = arr.filter(filterArray);
+sum = arr.filter(filterArray);
 
-// console.log(sum.length);
+console.log(sum.length);
 
 // Использование метода массива reduce:
-
-// let arr = ['е', 'о', 'г', 'д'];
-
-// console.log(arr);
 
 let sumReduce = arr.reduce((summa, it) => 
   summa + (it == 'а' || it == 'А' ||
@@ -59,16 +55,3 @@ let sumReduce = arr.reduce((summa, it) =>
   it == 'я' || it == 'Я'), 0);
 
 console.log(sumReduce);
-
-// let arr = [1, 2, 3, 4, 5];
-
-// let result = arr.reduce(function(sum, current) {
-//   return sum + current}, 0);
-
-// console.log(result);
-
-// let arr = [1, 2, 3, 4, 5];
-
-// let result = arr.reduce((sum, current) => sum + current, 0);
-
-// alert(result);
