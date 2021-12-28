925f072ab74bcc396565a3f7ec19daa1d38664a8
Igor <iharkrutoi@gmail.com>
2021-12-28 10:13:56 +0300

    task 14

---
+++ index.js
@@ -0,0 +1,12 @@
+import { bye } from './say.js';
+import { hi } from './say.js';
+
+// async function f() {
+    // let {hi, bye} = import ('./say.js');
+
+// let {hi, bye} = await import('./say.js');
+hi('Jon1');
+bye('Pet2');
+// }
+
+// f();
---
+++ say.js
@@ -0,0 +1,11 @@
+export function say(name) {
+    alert(`Привет ${name}`);
+}
+  
+export function bye(name) {
+    alert(`Пока ${name}`);
+}
+
+export function hi(name) {
+    alert(`Hello ${name}`);
+}
